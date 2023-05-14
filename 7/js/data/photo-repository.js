3c7bad0135273716668b2c42c376780e681c77bd
Keks <keks@htmlacademy.ru>
2023-05-15 01:01:59 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/data/photo-repository.js
@@ -0,0 +1,41 @@
+import { getRangomInt } from './utils/number-utils.js';
+
+const MAX_PHOTO_COUNT = 25;
+const MAX_LIKES_COUNT = 200;
+const MAX_COMMENTS_COUNT = 200;
+const MIN_LIKES_COUNT = 15;
+const MIN_COMMENTS_COUNT = 0;
+
+const getPhotos = function (count) {
+  if (typeof count !== 'number') {
+    throw new TypeError();
+  }
+
+  if (count <= 0) {
+    throw new RangeError('Count must be more than zero');
+  }
+
+  const correctCount = Math.min(count, MAX_PHOTO_COUNT);
+  const photos = new Array(correctCount);
+
+  for (let i = 0; i < photos.length; i++) {
+    const photoId = i + 1;
+
+    photos[i] = (
+      {
+        id: photoId,
+        url: `photos/${photoId}.png`,
+        description: `Фотография №/${photoId}`,
+        likes: getRangomInt(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
+        comments: getRangomInt(MIN_COMMENTS_COUNT, MAX_COMMENTS_COUNT)
+      }
+    );
+  }
+
+  return photos;
+};
+
+export {
+  getPhotos,
+  MAX_PHOTO_COUNT
+};
---
+++ 7/js/ui/photo-ui.js
@@ -0,0 +1,21 @@
+import { getPhotos, MAX_PHOTO_COUNT } from './data/photo-repository.js';
+
+const picturesFragment = document.createDocumentFragment();
+const pictures = document.querySelector('.pictures');
+const currentPicture = document.querySelector('#picture').content.querySelector('.picture');
+
+const photos = getPhotos(MAX_PHOTO_COUNT);
+
+photos.forEach(({url, likes, comments})=>{
+
+  const currentPictureClone = currentPicture.cloneNode(true);
+
+  currentPictureClone.querySelector('.picture__img').src = url;
+  currentPictureClone.querySelector('.picture__likes').textContent = likes;
+  currentPictureClone.querySelector('.picture__comments').textContent = comments;
+
+  picturesFragment.appendChild(currentPictureClone);
+});
+
+pictures.appendChild(picturesFragment);
+//Задание 7 часть 2
