49cf4562b3df3c6a41851ce4be57eff7595d85c2
Anadol <terehov.bogdan01@gmail.com>
2023-05-14 22:22:49 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -31,3 +31,40 @@ function validateTextLength(text, maxLength) {
 
   return text.length <= maxLengthInt;
 }
+
+const MAX_PHOTO_COUNT = 25;
+const MAX_LIKES_COUNT = 200;
+const MAX_COMMENT_COUNT = 200;
+const MIN_LIKES_COUNT = 15;
+const MIN_COMMENT_COUNT = 0;
+
+function getPhotos(count) {
+  if (typeof count !== 'number') {
+    throw new TypeError();
+  }
+
+  if (count <= 0) {
+    throw new RangeError('Count must be more than zero');
+  }
+
+  const correctCount = Math.min(count, MAX_PHOTO_COUNT);
+  const photos = new Array(correctCount);
+
+  for (let i = 0; i < photos.length; i++) {
+    const photoId = i + 1;
+
+    photos[i] = (
+      {
+        id: photoId,
+        url: `photos/${photoId}.png`,
+        description: `Фотография №/${photoId}`,
+        likes: getRangomInt(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
+        comments: getRangomInt(MIN_COMMENTS_COUNT, MAX_COMMENTS_COUNT)
+      }
+    );
+  }
+
+  return photos;
+}
+
+getPhotos(MAX_PHOTO_COUNT);
