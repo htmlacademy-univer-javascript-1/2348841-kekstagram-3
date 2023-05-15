f3e5e3db10d692b17aeb77ca55f64404ebcc377a
Anadol <terehov.bogdan01@gmail.com>
2023-05-15 04:47:49 +0300

    done tast from module 8

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 import './ui/photo-ui.js';
+import { setupUploadView } from './ui/upload-photo-ui.js';
+
+const MAX_DESCRIPTION_LENGTH = 20;
+const MIN_DESCRIPTION_LENGTH = 140;
+
+setupUploadView(MIN_DESCRIPTION_LENGTH, MAX_DESCRIPTION_LENGTH);
