5ec452c8857f733ec8b7573d3e3e72113b53dd6b
Keks <keks@htmlacademy.ru>
2023-05-15 04:48:56 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/ui/upload-photo-ui.js
@@ -0,0 +1,64 @@
+import 'pristine/pristine.min.js';
+
+const uploadInput = document.getElementById('upload-file');
+const uploadOverlay = document.querySelector('.img-upload__overlay');
+const cancelButton = document.querySelector('.img-upload__cancel');
+const body = document.querySelector('body');
+const uploadSelectImage = document.getElementById('upload-select-image');
+
+const pristine = new Pristine(uploadSelectImage, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'item--invalid',
+  successClass: 'item--valid',
+  errorTextParent: 'img-upload__field-wrapper',
+}
+);
+
+function handleKeydownEvent(event) {
+  if (event.key === 'Escape') {
+  //  cancelUpload();
+  }
+}
+
+function startUpload() {
+  uploadOverlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', handleKeydownEvent);
+}
+
+function cancelUpload() {
+  uploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', handleKeydownEvent);
+  uploadSelectImage.reset();
+}
+
+function validateDescription(description, minDescriptionLength, maxDescriptionLength) {
+  return description.length >= minDescriptionLength && description.length <= maxDescriptionLength;
+}
+
+function setupUploadView(minDescriptionLength, maxDescriptionLength) {
+
+  uploadInput.addEventListener('change', startUpload);
+  cancelButton.addEventListener('click', cancelUpload);
+
+  const descriptionTitle = `От ${minDescriptionLength} до ${maxDescriptionLength} символов`;
+
+  pristine.addValidator(
+    uploadSelectImage.querySelector('.text__description'),
+    (text) => {
+      validateDescription(text, minDescriptionLength, maxDescriptionLength);
+    },
+    descriptionTitle
+  );
+
+  uploadSelectImage.addEventListener('submit', (event) => {
+    if (!pristine.validate()) {
+      event.preventDefault();
+    }
+  });
+}
+
+export {
+  setupUploadView
+};
